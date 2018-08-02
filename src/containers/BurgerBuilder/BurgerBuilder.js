0a64d26b677e99feba648b54666e349de6a49eab
Vladimir Vladimirov <v.vladimirov@deltastock.com>
2018-08-02 12:02:44 +0300

    BuildControls Added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 
 import ReactAux from '../../hoc/ReactAux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render () {
         return (
             <ReactAux>
-                <Burger />
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
             </ReactAux>
         );
     }
